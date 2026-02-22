fc1598308f000e2bb2dc463132a403c9dedcd13f
JitenRaj <ji10raj@gmail.com>
2026-02-23 00:49:16 +0530

    Refactor: update test class name and add ActiveProfiles annotation

--- src/test/java/com/githubactivity/GithubactivityApplicationTests.java
+++ src/test/java/com/githubactivity/GithubactivityApplicationTests.java
@@ -2,9 +2,11 @@ package com.githubactivity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class GithubactivityApplicationTests {
+@ActiveProfiles("test") // This activates the test profile, skipping the CLI loop!
+class GithubActivityApplicationTests {
 
 	@Test
 	void contextLoads() {
